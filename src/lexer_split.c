fbe47c91d2661a42b96cad409d730dac453ecc2d
nok <visouthivong.kevin@gmail.com>
2025-03-09 16:02:19 +0100

    lexer seems OK

--- src/lexer_split.c
+++ src/lexer_split.c
@@ -6,11 +6,26 @@
 /*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 15:53:04 by kevso             #+#    #+#             */
-/*   Updated: 2025/03/08 22:15:13 by kevso            ###   ########.fr       */
+/*   Updated: 2025/03/09 16:01:58 by kevso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+/* Updates the quote status */
+void	update_quote_status(t_lexer *lex, char c)
+{
+	if (c == '\'' || c == '\"')
+	{
+		if (lex->inside_quotes && c == lex->quote_char)
+			lex->inside_quotes = false;
+		else if (!lex->inside_quotes)
+		{
+			lex->inside_quotes = true;
+			lex->quote_char = c;
+		}
+	}
+}
 
 /* Just like a count_words, but ignores the spaces inside quotes */
 int	count_tokens(char *str, t_lexer *lex)
@@ -24,16 +39,7 @@ int	count_tokens(char *str, t_lexer *lex)
 	lex->inside_quotes = false;
 	while (str[i])
 	{
-		if (str[i] == '\'' || str[i] == '\"')
-		{
-			if (lex->inside_quotes && str[i] == lex->quote_char)
-				lex->inside_quotes = false;
-			else if (!lex->inside_quotes)
-			{
-				lex->inside_quotes = true;
-				lex->quote_char = str[i];
-			}
-		}
+		update_quote_status(lex, str[i]);
 		if (!lex->inside_quotes && str[i] == ' ')
 			count++;
 		i++;
@@ -66,7 +72,6 @@ char	**split_tokens(char *str, t_lexer *lex)
 			update_quote_status(lex, str[i++]);
 		if (i > start)
 			tokens[j++] = ft_substr(str, start, i - start);
-
 	}
 	tokens[j] = NULL;
 	return (tokens);
