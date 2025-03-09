31704ddf723fca08dd8cb2566be3eb9cfe4548e6
nok <visouthivong.kevin@gmail.com>
2025-03-09 16:55:44 +0100

    gestion cmd vide

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:07:48 by kevso             #+#    #+#             */
-/*   Updated: 2025/03/07 16:30:52 by kevso            ###   ########.fr       */
+/*   Updated: 2025/03/09 16:55:22 by kevso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 int	lexer(t_shell *shell)
 {
+	if (shell->cmdline[0] == '\0' || shell->cmdline[0] == '\n')
+		return (0);
 	shell->new_cmdline = add_spaces_to_cmdline(shell->cmdline, &shell->lexer);
 	if (!shell->new_cmdline)
 		return (0);
-	printf("\nnew_cmdline: %s\n\n", shell->new_cmdline);
 	shell->lexer.token_count = count_tokens(shell->new_cmdline, &shell->lexer);
-	printf("token_count: %d\n\n", shell->lexer.token_count);
 	shell->lexer.tokens = split_tokens(shell->new_cmdline, &shell->lexer);
 	if (!shell->lexer.tokens)
 		return (0);
-	int i = 0;
-	while (shell->lexer.tokens[i])
-	{
-		printf("token[%d]: %s\n", i, shell->lexer.tokens[i]);
-		i++;
-	}
 	return (1);
 }
 
