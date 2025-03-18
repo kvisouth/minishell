5bcd61d61e9c1e9fcd2c756073f9734f1613e2b9
nok <visouthivong.kevin@gmail.com>
2025-03-18 17:44:16 +0100

    cd

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 15:39:40 by kevso             #+#    #+#             */
-/*   Updated: 2025/03/18 16:12:06 by kevso            ###   ########.fr       */
+/*   Updated: 2025/03/18 16:46:40 by kevso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 // cd with only a relative or absolute path
 int	builtin_cd(t_shell *shell, t_simple_cmds *cmd)
 {
-	// char	cwd[1024];
+	char	cwd[1024];
 	char	oldpwd[1024];
 
 	getcwd(oldpwd, sizeof(oldpwd));
+	if (cmd->str[1] == NULL || cmd->str[2] != NULL)
+		return (1);
 	(void)cmd;
 	(void)shell;
 	return (0);
