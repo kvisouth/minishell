a3350e26c71408a64d76f9f8fc3ca1e4365a7195
nok <visouthivong.kevin@gmail.com>
2025-03-21 16:22:07 +0100

    cd

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,13 +6,50 @@
 /*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 15:39:40 by kevso             #+#    #+#             */
-/*   Updated: 2025/03/18 16:46:40 by kevso            ###   ########.fr       */
+/*   Updated: 2025/03/21 16:21:48 by kevso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// cd with only a relative or absolute path
+void	update_env(t_shell *shell, char *tmp)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = ft_substr(tmp, 0, ft_strchr(tmp, '=') - tmp);
+	while (shell->env[i])
+	{
+		if (!ft_strncmp(shell->env[i], str, ft_strlen(str)))
+		{
+			free(shell->env[i]);
+			shell->env[i] = ft_strdup(tmp);
+			break ;
+		}
+		i++;
+	}
+	free(str);
+}
+
+void	update_oldpwd(t_shell *shell, char *oldpwd)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin("OLDPWD=", oldpwd);
+	update_env(shell, tmp);
+	free(tmp);
+}
+
+void	update_pwd(t_shell *shell, char *cwd)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin("PWD=", cwd);
+	update_env(shell, tmp);
+	free(tmp);
+}
+
 int	builtin_cd(t_shell *shell, t_simple_cmds *cmd)
 {
 	char	cwd[1024];
@@ -21,7 +58,16 @@ int	builtin_cd(t_shell *shell, t_simple_cmds *cmd)
 	getcwd(oldpwd, sizeof(oldpwd));
 	if (cmd->str[1] == NULL || cmd->str[2] != NULL)
 		return (1);
-	(void)cmd;
-	(void)shell;
+	if (chdir(cmd->str[1]) == -1)
+	{
+		perror("cd");
+		return (1);
+	}
+	else
+	{
+		getcwd(cwd, sizeof(cwd));
+		update_oldpwd(shell, oldpwd);
+		update_pwd(shell, cwd);
+	}
 	return (0);
 }
