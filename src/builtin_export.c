642e57fec8aebc251a653e011487f3e7967d798d
nok <visouthivong.kevin@gmail.com>
2025-04-12 09:45:55 +0200

    working

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nok <nok@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 17:09:05 by kevso             #+#    #+#             */
-/*   Updated: 2025/04/11 02:35:31 by kevso            ###   ########.fr       */
+/*   Updated: 2025/04/12 09:45:37 by nok              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,62 @@ int	check_export_args(t_simple_cmds *cmd)
 	return (1);
 }
 
-int	append_str_to_env(t_shell *shell, char *str)
+/*
+Checks if the variable already exists in the environment
+1 : variable already exists
+0 : variable doesn't exist
+*/
+int	check_var_already_exist(t_shell *shell, char *var)
+{
+	int	i;
+
+	i = 0;
+	while (shell->env[i])
+	{
+		if (ft_strncmp(shell->env[i], var,
+				ft_strchr(shell->env[i], '=') - shell->env[i]) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*
+Deletes the line of 'var' from the environment
+Returns the new environment without that variable
+*/
+int	delete_var_env(t_shell *shell, char *var)
+{
+	int		i;
+	int		j;
+	char	**new_env;
+
+	i = 0;
+	j = 0;
+	while (shell->env[i])
+		i++;
+	new_env = malloc(sizeof(char *) * i);
+	if (!new_env)
+		return (0);
+	i = 0;
+	while (shell->env[i])
+	{
+		if (ft_strncmp(shell->env[i], var,
+				ft_strchr(shell->env[i], '=') - shell->env[i]) != 0)
+			new_env[j++] = shell->env[i];
+		i++;
+	}
+	new_env[j] = NULL;
+	free(shell->env);
+	shell->env = new_env;
+	return (1);
+}
+
+/*
+Adds the variable 'var' to the environment
+Returns the new environment with that variable
+*/
+int	add_var_env(t_shell *shell, char *var)
 {
 	int		i;
 	char	**new_env;
@@ -53,23 +108,18 @@ int	append_str_to_env(t_shell *shell, char *str)
 		i++;
 	new_env = malloc(sizeof(char *) * (i + 2));
 	if (!new_env)
-		return (1);
+		return (0);
 	i = 0;
 	while (shell->env[i])
 	{
-		new_env[i] = ft_strdup(shell->env[i]);
+		new_env[i] = shell->env[i];
 		i++;
 	}
-	new_env[i] = ft_strdup(str);
+	new_env[i] = ft_strdup(var);
 	new_env[i + 1] = NULL;
-	if (!new_env[i] || !new_env[i + 1])
-	{
-		free_tab(new_env);
-		return (1);
-	}
-	free_tab(shell->env);
+	free(shell->env);
 	shell->env = new_env;
-	return (0);
+	return (1);
 }
 
 int	builtin_export(t_shell *shell, t_simple_cmds *cmd)
@@ -78,8 +128,17 @@ int	builtin_export(t_shell *shell, t_simple_cmds *cmd)
 		return (ft_putstr_fd("incorrect format\n", 2), 1);
 	if (check_export_args(cmd) == 2)
 		return (0);
-	printf("%s\n", cmd->str[1]);
-	if (append_str_to_env(shell, cmd->str[1]))
-		return (1);
+	if (check_var_already_exist(shell, cmd->str[1]))
+	{
+		if (!delete_var_env(shell, cmd->str[1]))
+			return (ft_putstr_fd("malloc error\n", 2), 1);
+		if (!add_var_env(shell, cmd->str[1]))
+			return (ft_putstr_fd("malloc error\n", 2), 1);
+	}
+	else
+	{
+		if (!add_var_env(shell, cmd->str[1]))
+			return (ft_putstr_fd("malloc error\n", 2), 1);
+	}
 	return (0);
 }
